afcb488a723d76f08e8d59c01d7f3e11077d589f
artem <artem.yurev.1997@gmail.com>
2023-03-13 17:07:53 +0300

    Initial commit

---
+++ src/by/pvt/lessonthree/Task.java
@@ -0,0 +1,56 @@
+package by.pvt.lessonthree;
+
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*                                           Задача 1
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if(a % 2 == 0) {
+            System.out.println(a);
+        }
+        if(b % 2 == 0) {
+            System.out.println(b);
+        }
+        if(c % 2 == 0) {
+            System.out.println(c);
+        }
+        */
+
+
+        /*
+        int a = scanner.nextInt();                   Задача 2
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if(a % 2 == 0 && a % 5 == 0) {
+            System.out.println(a);
+        }
+        if (b % 2 == 0 && b % 5 == 0) {
+            System.out.println(b);
+        }
+        if(c % 2 == 0 && c % 5 == 0) {
+            System.out.println(c);
+        }
+        */
+
+        /*                                        //Задача 3
+        int x = scanner.nextInt();
+        int result = (int) Math.round(15.0 / x);
+        switch(result) {
+            case 3:
+                System.out.println("Результат деления равен 3");
+                break;
+            case 5:
+                System.out.println("Результат деления равен 5");
+                break;
+            default:
+                double d = 15.0 / x;
+                System.out.println(d);
+        }
+        */
+    }
+}
